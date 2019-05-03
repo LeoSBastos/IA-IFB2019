fb387224701893e7968cbbdac4cb5c4c2bf370ab
Jeronimo <jeronimohermano.nc@gmail.com>
2019-05-03 00:06:38 -0300

    Formatação do arquivo data, mudança na leitura e correção das funções custo e normalizar

--- t2 - Regressão linear/__main__.py
+++ t2 - Regressão linear/__main__.py
@@ -1,5 +1,17 @@
 from regressao import RegressaoLinear as rl
 
+
+"""
+O arquivo data.txt possui a seguinte formatação
+ - A primeira linha informa o número de colunas nas linhas de dados
+ - A segunda linha informa o número total de amostras
+ - As linhas 3-6 indicam o significado de cada coluna na amostra de dados
+
+ - Coluna 0: indice da linha, em seu lugar será salvo o valor 1 para X0
+ - Coluna 2: idade do peixe, valor de X1
+ - Coluna 3: temperatura da água em C°, valor de X2
+ - Coluna 4: tamanho do peixe, Y
+"""
 def reader(file_name):
     # Lista com todos os Xs e Ys utilizados para o aprendizado
     x, y = [], []
@@ -8,30 +20,35 @@ def reader(file_name):
         # Lê o arquivo e cria um vetor com todas as linhas dele
         linhas = file.read().split('\n')
         # Lê o número de colunas
-        colunas = int(linhas[0][0])
+        colunas = int(linhas[0])
         # Lista de X indo de X0 ate Xn-1
         x.append( (colunas - 1) * list())
         # Leitura dos parâmetros, que começam na linha 6
         for l in linhas[6:]:
+            # Particiona a linha em vetores
             c = l.split()
-            for i in range(colunas+1):
-                # Y
-                if i == colunas:
-                    y.append(int(c[i]))
-                # X0
-                elif i == 0:
+            # Percorre os vetores pelos ídices
+            for i in range(colunas):
+                # X0 - A coluna 0 é o index
+                if i == 0:
                     x[0].append(int(c[0]))
                 # XN com N > 0
+                elif 0 < i < (colunas - 1):
+                    x[i].append(int(c[i]))
+                # Y - A última coluna é o 
                 else:
-                    x[i - 1].append(int(c[0]))
+                    y.append(int(c[i]))
+    # retorna a matriz de valor X e os Y resultados
     return x, y
-    
-if __name__ == "__main__":
-    x, y = reader('data.txt')
-    
-    def h(teta, x): return sum([(teta[i]*x[i]) for i in range(len(x))])
 
-    r = rl(x, y, h)
+if __name__ == "__main__":
+    # Chama a função para ler o arquivo e retornar os vetores X e Y
+    x, y = reader('data.1.txt')
+    
+    # Funcção C: calcula o tamanho do peixe com base na idade e temperatura da agua
+    def c(teta, x): return sum([(teta[i]*x[i]) for i in range(len(teta))])
+
+    r = rl(x, y, c)
     #r.solve()
     #print(r.test([1, 2104, 3]))
     print(r.x, r.y)
--- t2 - Regressão linear/regressao.py
+++ t2 - Regressão linear/regressao.py
@@ -2,41 +2,47 @@ class RegressaoLinear(object):
     MAX_INTERACOES = 5000
     MIN_CUSTO = 1E-5
 
-    def __init__(self, x, y, h, teta=[1]*len(x), alfa=0.00000019):
-        self.x = x
-        self.y = y
-        self.h = h
-        self.m = len(self.y)
-        self.teta = teta
-        self.alfa = alfa
-        self.delta = self.diferenca()
-        self.media = self.avg()
-
-    def diferenca(self):
-        r = 1
-        for n in self.x[1:]:
-            r.append(max(n)-min(n))
-        return r
+    def __init__(self, x, y, h, teta=[1]*len(x), alfa=0.00000019, normaliza = True):
+        self.x = x # Matriz de variáveis para aprendizado X
+        self.y = y # Matriz de resultado para aprendizado X
+        self.h = h # Funcao de hipótese
+        self.m = len(self.y) # Quantidade de amostras
+        self.teta = teta # Constantes da função: que serão otmizados pela regressão
+        self.alfa = alfa # Taxa de aprendizagem
+        if normaliza:
+            self.x = self.normalizar(x)
+            self.y = self.normalizar(y)
 
     """
-    AVG - Calcula a média de todos os parâmetros de X
+    NORMALIZAR - Normaliza os valores de uma matriz
+     - A normalização é aplicada linha a linha
     """
-    def avg(self):
-        r = [1]
-        for n in self.x[1:]:
-            r.append(sum(n) / len(r))
-        return r
-
-    def normaliza(self):
-        # Normaliza X
-        for i in range(1, len(self.x)):
-            for j in range(len(self.x[j])):
-                self.x[i][j] = self.x[i][j] * (self.media[i]/self.delta[i])
-        # Normaliza Y
-        pass
-
+    def normalizar(self, v):
+        delta = list()
+        media = list()
+        # Para todas as linhas de V
+        for i in v:
+            # calcula a media de cada parametro
+            media.append(sum(i) / m)
+            # calcula a variacao de cada parametro
+            delta.append(max(i) - min(i))
+        # Para cada linha de V
+        for i in range(1, len(v)):
+            # Normaliza cada item da linha
+            for j in range(m):
+                # Vi' = (Vi - MEDIAi) / DELTAi
+                v[i][j] = (v[i][j] - media[i]) / delta[i]
+        return v
+    """
+    CUSTO - Calcula o custo total para os testes com os atuais valores de TETA
+     J = SUM( H(teta, x) - Y ) / 2M
+    """
     def custo(self):
-        return sum([(self.h(self.teta, self.x[i]) - self.y[i]) ** 2 for i in range(self.m)]) / (2 * self.m)
+        c = 0
+        # sum([(self.h(self.teta, self.x[i]) - self.y[i]) ** 2 for i in range(self.m)])
+        for i in range(self.m):
+            c += (self.h(self.teta, [self.x[i][j] for j in range(self.x)]) - self.y[i]) ** 2
+        return c / (2 * self.m)
 
     def atualiza_teta(self):
         novo = list() 
@@ -63,15 +69,6 @@ class RegressaoLinear(object):
             i += 1
         pass
 
-    # def test(self, x):
-    #     return self.h(self.teta, x)
-    #         # Atualiza os tetas da equação
-    #         self.atualiza_teta()
-    #         # Incrementa o contador de iterações
-    #         i += 1
-    #     pass
-
+    # Função que possibilita realizar teste com a funçãoe criada a partir da regressão
     def test(self, x):
         return self.h(self.teta, x)
-
-
