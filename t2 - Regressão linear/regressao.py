2e5f9afc9f3ebf2e1f893c7b182b4ba258fd2eab
Jeronimo <jeronimohermano.nc@gmail.com>
2019-05-03 00:48:04 -0300

    Correção dos índices de X na função custo

--- t2 - Regressão linear/regressao.py
+++ t2 - Regressão linear/regressao.py
@@ -1,5 +1,7 @@
 class RegressaoLinear(object):
+    # Número máximo de vezes que será aplicada a regressao linear
     MAX_INTERACOES = 5000
+    # Custo mínimo, é uma das condições de parada do método solve()
     MIN_CUSTO = 1E-5
 
     def __init__(self, x, y, h, teta=[1]*len(x), alfa=0.00000019, normaliza = True):
@@ -7,7 +9,7 @@ class RegressaoLinear(object):
         self.y = y # Matriz de resultado para aprendizado X
         self.h = h # Funcao de hipótese
         self.m = len(self.y) # Quantidade de amostras
-        self.teta = teta # Constantes da função: que serão otmizados pela regressão
+        self.teta = teta # Constantes da função: que serão otmizados pela regressao
         self.alfa = alfa # Taxa de aprendizagem
         if normaliza:
             self.x = self.normalizar(x)
@@ -16,6 +18,7 @@ class RegressaoLinear(object):
     """
     NORMALIZAR - Normaliza os valores de uma matriz
      - A normalização é aplicada linha a linha
+     - Vi' = (Vi - MEDIAi) / DELTAi
     """
     def normalizar(self, v):
         delta = list()
@@ -30,7 +33,6 @@ class RegressaoLinear(object):
         for i in range(1, len(v)):
             # Normaliza cada item da linha
             for j in range(m):
-                # Vi' = (Vi - MEDIAi) / DELTAi
                 v[i][j] = (v[i][j] - media[i]) / delta[i]
         return v
     """
@@ -39,9 +41,13 @@ class RegressaoLinear(object):
     """
     def custo(self):
         c = 0
-        # sum([(self.h(self.teta, self.x[i]) - self.y[i]) ** 2 for i in range(self.m)])
+        # Peercorre os M itens da amostra
         for i in range(self.m):
-            c += (self.h(self.teta, [self.x[i][j] for j in range(self.x)]) - self.y[i]) ** 2
+            # Passa como parametros a variável teta e uma lista com todos os Xji
+            # e depois subtrai o Yi
+            #   - i: linha do caso de teste
+            #   - j: índice do X (x0, x1, x2, etc)
+            c += (self.h(self.teta, [self.x[j][i] for j in range(self.x)]) - self.y[i]) ** 2
         return c / (2 * self.m)
 
     def atualiza_teta(self):
@@ -55,12 +61,16 @@ class RegressaoLinear(object):
         print(novo)
         self.teta = novo
 
+    """
+    SOLVE: aplica a regressão linear para encontrar os parâmetros TETA que melhor se ajustam
+    - Realiza um número máximo de interações delimitado por MAX_INTERACOES 
+    """
     def solve(self):
         i = 0
         while i < self.MAX_INTERACOES:
             # Calcula o custo atual
             custo = self.custo()
-            # Para os loops caso o custo seja suficientemente pequeno
+            # Encerra os loops caso o custo seja suficientemente pequeno
             if custo < self.MIN_CUSTO:
                 break
             # Atualiza os tetas da equação
