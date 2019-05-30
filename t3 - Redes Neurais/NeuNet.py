de527f1ad60e801bcc0d6dfbbe8d5113f43bf22e
Jeronimo <jeronimohermano.nc@gmail.com>
2019-05-30 16:17:06 -0300

    Finetuning e parsing dos argumentos

--- t3 - Redes Neurais/NeuNet.py
+++ t3 - Redes Neurais/NeuNet.py
@@ -7,63 +7,77 @@ from tensorflow import keras
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 import os
 from pathlib import Path
 
 class NeuNet:
-    def __init__(self,train_x,train_y,test_x,plot_linha=5, plot_coluna=5):
+    def __init__(self,train_x,train_label,test_x,test_label,epochs,ti,plot_linha=5, plot_coluna=5):
         self.plot_linha=plot_linha
         self.plot_coluna=plot_coluna
         self.train_x=train_x
-        self.train_y=train_y
+        self.train_label=train_label
         self.test_x=test_x
+        self.test_label=test_label
+        self.epochs=epochs
+        self.total_images=ti
         self.classes=range(10)
         self.train_x.shape
         self.model = keras.Sequential([
             keras.layers.Flatten(input_shape=(28,28)),
-            keras.layers.Dense(784,activation=tf.nn.sigmoid),
+            keras.layers.Dense(784,activation=tf.nn.relu),
             keras.layers.Dense(392,activation=tf.nn.sigmoid),
             keras.layers.Dense(392,activation=tf.nn.sigmoid),
             keras.layers.Dense(10, activation=tf.nn.sigmoid)
         ])
-        self.model.compile(optimizer="adam",
+        self.model.compile(optimizer="adagrad",#"adam",
                     loss="sparse_categorical_crossentropy",
                     metrics=["accuracy"])
-        self.model.fit(train_x,train_y,epochs=3)
+        self.model.fit(train_x,train_label,epochs=self.epochs)
         self.predictions = self.model.predict(test_x)
 
-    def plot_image(self,i, predictions_array, img):
-        predictions_array, img = predictions_array[i], img[i]
-        plt.grid(False)
-        plt.xticks([])
-        plt.yticks([])
-        plt.imshow(img, cmap=plt.cm.binary)
+    
+    def plot_image(self,i, predictions_array, true_label, img):
+      predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+      plt.grid(False)
+      plt.xticks([])
+      plt.yticks([])
+      
+      plt.imshow(img, cmap=plt.cm.binary)
+    
+      predicted_label = np.argmax(predictions_array)
+      if predicted_label == true_label:
+        color = 'blue'
+      else:
+        color = 'red'
+      
+      plt.xlabel("IMG N{}: {} {:2.0f}% ({})".format(i,self.classes[predicted_label],
+                                    100*np.max(predictions_array),
+                                    self.classes[true_label]),
+                                    color=color)
 
-        predicted_label = np.argmax(predictions_array)
-        plt.xlabel("{} {:2.0f}%".format(self.classes[predicted_label],
-                                        100*np.max(predictions_array),
-                                        color='black'))                            
-
-    def plot_value_array(self,i, predictions_array):
-        predictions_array = predictions_array[i]
-        plt.grid(False)
-        plt.xticks([])
-        plt.yticks([])
-        thisplot = plt.bar(range(10), predictions_array, color="#777777")
-        plt.ylim([0, 1]) 
-        self.predicted_label = np.argmax(predictions_array)
-        thisplot[self.predicted_label].set_color('blue')
+    def plot_value_array(self, i, predictions_array, true_label):
+      predictions_array, true_label = predictions_array[i], true_label[i]
+      plt.grid(False)
+      plt.xticks([])
+      plt.yticks([])
+      thisplot = plt.bar(range(10), predictions_array, color="#777777")
+      plt.ylim([0, 1]) 
+      predicted_label = np.argmax(predictions_array)
+      thisplot[predicted_label].set_color('red')
+      thisplot[true_label].set_color('blue')
 
     def PlotGraph(self):
         num_images = self.plot_linha*self.plot_coluna
         my_path = os.path.abspath(__package__)
-        for j in range(0,len(self.test_x),num_images):
-            file_name = "figs/fig{}.png".format(j)
+        for j in range(self.total_images):
+            file_name = "figs/fig{}.png".format(j+1)
             plt.figure(figsize=(2*2*self.plot_coluna, 2*self.plot_linha))
             for i in range(num_images):
+                aux=random.randint(0,len(self.test_x))
                 plt.subplot(self.plot_linha, 2*self.plot_coluna, 2*i+1)
-                self.plot_image(j+i, self.predictions, self.test_x)
+                self.plot_image(aux, self.predictions, self.test_label, self.test_x)
                 plt.subplot(self.plot_linha, 2*self.plot_coluna, 2*i+2)
-                self.plot_value_array(j+i, self.predictions)
+                self.plot_value_array(aux, self.predictions,self.test_label)
             p = Path(os.path.join(my_path,file_name)).resolve()
             plt.savefig(str(p))
--- t3 - Redes Neurais/__main__.py
+++ t3 - Redes Neurais/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import numpy.linalg as LA
 from NeuNet import NeuNet
@@ -13,7 +14,7 @@ O arquivo data.txt possui a seguinte formatação
  - Coluna 4: tamanho do peixe, Y
 """
 
-def readerTrain(file_name,n):
+def reader(file_name,n):
     # Lista com todos os Xs e Ys utilizados para o aprendizado
     y = []
     # Leitura do arquivo
@@ -37,38 +38,31 @@ def readerTrain(file_name,n):
     for i in range(len(aux)):
         final[i]=(aux[i]/LA.norm(aux[i]))
     # retorna a matriz de valor X e os Y resultados
-    return final, y
+    return final, np.array(y)
 
-def readerTest(file_name,n):
-    # Lista com todos os Xs e Ys utilizados para o aprendizado
-    # Leitura do arquivo
-    with open(file_name, 'r') as file:
-        # Lê o arquivo e cria um vetor com todas as linhas dele
-        linhas = file.read().split('\n')
-        # Lista de X indo de X0 ate Xn-1
-        x= [[[] for i in range(28)] for i in range(n)]
-        # Leitura dos parâmetros, que começam na linha 6
-        for i,l in enumerate(linhas[1:]):
-            # Particiona a linha em vetores
-            c = l.split(",")
-            # Percorre os vetores pelos ídices
-            for j in range(28):
-                for k in range(28):
-                    x[i][j].append(int(c[28*j+k]))
-    # retorna a matriz de valor X
-    aux=np.array(x)
-    final = np.empty([n,28,28])
-    for i in range(len(aux)):
-        final[i]=(x[i]/LA.norm(aux[i]))
-    return final
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+    description="Trabalho 3 de Inteligência Computacional - Treinamento de Redes Neurais")
+    parser.add_argument("TRF", metavar="train_filename",
+    type=str,nargs='?',default="train.csv",help="O nome do arquivo contendo as imagens de treino, default: train.csv")
+    parser.add_argument("TRN",metavar="train_number",type=int,nargs='?',
+    help="Número de imagens a serem treinadas")
+    parser.add_argument("TSF", metavar="test_filename",
+                        type=str, nargs='?', default="test.csv", help="O nome do arquivo contendo as imagens de teste, default: test.csv")
+    parser.add_argument("TSN",metavar="test_number",type=int,nargs='?',
+    help="Número de imagens a serem testadas")
+    parser.add_argument("--epochs",metavar="epochs",type=int,nargs='?',default=5,
+    dest="epochs",help="O número de iterações de treinamento, default: 5")
+    parser.add_argument("--ti",metavar="total_images",type=int,nargs='?',default=1,
+    dest="ti",help="O número de imagens de 5x5 figuras geradas, default: 1")
+    args = parser.parse_args()
     # Chama a função para ler o arquivo e retornar os vetores X e Y
-    train_x, train_y = readerTrain('train.csv',42000)
-    test_x = readerTest('test.csv',28000)
+    train_x, train_label = reader(args.TRF,args.TRN)
+    test_x, test_label = reader(args.TSF,args.TSN)
     
     #Crie a classe de Regressão Linear com os vetores x, y e a função custo C
-    rede = NeuNet(train_x, train_y, test_x)
+    rede = NeuNet(train_x, train_label, test_x, test_label,args.epochs,args.ti)
     #Utiliza a função solve que atualiza os tetas e a função custo de acordo com o número de iterações que foram selecionadas.
     rede.PlotGraph()
