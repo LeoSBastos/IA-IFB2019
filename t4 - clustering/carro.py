fa6bd3c2aa633b87a69a5776e14d36bd39842996
Leonardo Bastos <31369096+LeoSBastos@users.noreply.github.com>
2019-06-28 17:45:53 -0300

    Comment

--- t4 - clustering/carro.py
+++ t4 - clustering/carro.py
@@ -3,9 +3,10 @@
 """
 Created on Tue Jun 11 13:46:48 2019
 
-@author: jeronimo
+@author: Jeronimo Hermano e Leonardo Bastos
 """
-
+#Definicao da classe carro que tem seu modelo, eixo x(Carbono),
+#eixo y(Milhagem) e sua classe(Centroid).
 class Carro():
     
     def __init__(self, modelo, carbono, milhagem):
